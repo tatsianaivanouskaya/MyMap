41d51dff3b53fb60eb26be387ebc3a26cb9b7864
Tatjana Ivanovskaya <salamandra.00@mail.ru>
2019-11-20 15:07:50 +0300

    databinding

--- app/src/main/java/com/example/mymap/presenter/MarkerAdapter.java
+++ app/src/main/java/com/example/mymap/presenter/MarkerAdapter.java
@@ -30,8 +30,10 @@ public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder
     public void onBindViewHolder(final ViewHolder holder, final int position) {
 
         final MarkerInfo itemList = markerItems.get(position);
-        holder.txtLatitude.setText("ш. " + itemList.getLatitude().toString());
-        holder.txtLonitude.setText("д. " + itemList.getLongitude().toString());
+        String txtLat = mContext.getString(R.string.lat) + itemList.getLatitude();
+        String txtLon = mContext.getString(R.string.lon) + itemList.getLongitude();
+        holder.txtLatitude.setText(txtLat);
+        holder.txtLongitude.setText(txtLon);
         holder.txtCountry.setText(itemList.getCountry());
         holder.txtCity.setText(itemList.getAdministrativeArea());
     }
@@ -43,14 +45,15 @@ public class MarkerAdapter extends RecyclerView.Adapter<MarkerAdapter.ViewHolder
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public TextView txtLatitude;
-        public TextView txtLonitude;
-        public TextView txtCountry;
-        public TextView txtCity;
-        public ViewHolder(View itemView) {
+        TextView txtLatitude;
+        TextView txtLongitude;
+        TextView txtCountry;
+        TextView txtCity;
+
+        ViewHolder(View itemView) {
             super(itemView);
             txtLatitude = itemView.findViewById(R.id.tv_hist_latitude);
-            txtLonitude = itemView.findViewById(R.id.tv_hist_longitude);
+            txtLongitude = itemView.findViewById(R.id.tv_hist_longitude);
             txtCountry = itemView.findViewById(R.id.tv_hist_country);
             txtCity = itemView.findViewById(R.id.tv_hist_city);
             itemView.setOnClickListener(this);
