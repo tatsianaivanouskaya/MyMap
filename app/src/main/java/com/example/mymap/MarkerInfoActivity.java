670bd6b7a2aa6db9b06eb8bf441f87e3f44f818b
Tatjana Ivanovskaya <salamandra.00@mail.ru>
2019-11-20 15:08:05 +0300

    databinding

--- app/src/main/java/com/example/mymap/MarkerInfoActivity.java
+++ app/src/main/java/com/example/mymap/MarkerInfoActivity.java
@@ -2,9 +2,9 @@ package com.example.mymap;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.widget.TextView;
-import com.example.mymap.model.MarkerInfoModel;
+import com.example.mymap.databinding.ActivityMarkerInfoBinding;
 import com.example.mymap.presenter.MarkerInfo;
 import com.example.mymap.presenter.MarkerInfoActivityPresenter;
 
@@ -14,32 +14,12 @@ public class MarkerInfoActivity extends AppCompatActivity implements MarkerInfoA
     public static final String MARKER_LATITUDE = "latitude";
     public static final String MARKER_LONGITUDE = "longitude";
 
-    TextView tvLatitude;
-    TextView tvLongitude;
-    TextView tvCountry;
-    TextView tvCity;
-    TextView tvDate;
-    TextView tvMaxTemp;
-    TextView tvMaxTempText;
-    TextView tvMinTemp;
-    TextView tvMinTempText;
     MarkerInfo markInfo;
-    MarkerInfoModel markerInfoModel;
     MarkerInfoActivityPresenter markerInfoActivityPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_marker_info);
-        tvLatitude = findViewById(R.id.tv_latitude);
-        tvLongitude = findViewById(R.id.tv_longitude);
-        tvCountry = findViewById(R.id.tv_country);
-        tvCity = findViewById(R.id.tv_city);
-        tvDate = findViewById(R.id.tv_date);
-        tvMaxTemp = findViewById(R.id.tv_maxTemp);
-        tvMaxTempText = findViewById(R.id.tv_maxTempText);
-        tvMinTemp = findViewById(R.id.tv_minTemp);
-        tvMinTempText = findViewById(R.id.tv_minTempText);
 
         final Double latitude = getIntent().getDoubleExtra(MARKER_LATITUDE, 0.0);
         final Double longitude = getIntent().getDoubleExtra(MARKER_LONGITUDE, 0.0);
@@ -47,25 +27,13 @@ public class MarkerInfoActivity extends AppCompatActivity implements MarkerInfoA
         markerInfoActivityPresenter = new MarkerInfoActivityPresenter(this);
         markerInfoActivityPresenter.attachView(this);
         markerInfoActivityPresenter.getMarkerInfo(latitude, longitude);
-
     }
 
     @Override
     public void getMarker(MarkerInfo markerInfo) {
         markInfo = markerInfo;
-        markerInfoModel = new MarkerInfoModel();
-        markerInfoModel.setMarker(markInfo, this);
-
-        tvLatitude.setText(markInfo.getLatitude().toString());
-        tvLongitude.setText(markInfo.getLongitude().toString());
-        tvCountry.setText(markInfo.getCountry());
-        tvCity.setText(markInfo.getAdministrativeArea());
-        tvDate.setText(getResources().getText(R.string.weather_at) + markerInfo.getDateWeather());
-        tvMaxTemp.setText(markInfo.getMaxTemp().toString());
-        tvMaxTempText.setText(markerInfo.getDayIconPhrase());
-        tvMinTemp.setText(markInfo.getMinTemp().toString());
-        tvMinTempText.setText(markInfo.getNightIconPhrase());
-
+        ActivityMarkerInfoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_marker_info);
+        binding.setMarkerinfo(markInfo);
     }
 
     @Override
