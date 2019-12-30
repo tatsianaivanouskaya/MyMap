b9ef9cd4acb980885892a6d8168cbb0a6bc69f51
Tatjana Ivanovskaya <salamandra.00@mail.ru>
2019-12-30 10:05:21 +0300

    adds SwipeRefreshLayout to MarkerInfoActivity

--- app/src/main/java/com/example/mymap/MarkerInfoActivity.java
+++ app/src/main/java/com/example/mymap/MarkerInfoActivity.java
@@ -3,27 +3,35 @@ package com.example.mymap;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Toast;
 import com.example.mymap.databinding.ActivityMarkerInfoBinding;
 import com.example.mymap.presenter.MarkerInfo;
 import com.example.mymap.presenter.MarkerInfoActivityPresenter;
 
 
-public class MarkerInfoActivity extends AppCompatActivity implements MarkerInfoActivityPresenter.UserMarkerActivity {
+public class MarkerInfoActivity extends AppCompatActivity implements MarkerInfoActivityPresenter.UserMarkerActivity, SwipeRefreshLayout.OnRefreshListener {
 
     public static final String MARKER_LATITUDE = "latitude";
     public static final String MARKER_LONGITUDE = "longitude";
 
+    private Double latitude;
+    private Double longitude;
+
     MarkerInfo markInfo;
     MarkerInfoActivityPresenter markerInfoActivityPresenter;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.progress_bar);
 
-        final Double latitude = getIntent().getDoubleExtra(MARKER_LATITUDE, 0.0);
-        final Double longitude = getIntent().getDoubleExtra(MARKER_LONGITUDE, 0.0);
+        latitude = getIntent().getDoubleExtra(MARKER_LATITUDE, 0.0);
+        longitude = getIntent().getDoubleExtra(MARKER_LONGITUDE, 0.0);
 
         markerInfoActivityPresenter = new MarkerInfoActivityPresenter(this);
         markerInfoActivityPresenter.attachView(this);
@@ -34,6 +42,14 @@ public class MarkerInfoActivity extends AppCompatActivity implements MarkerInfoA
     public void getMarker(MarkerInfo markerInfo) {
         markInfo = markerInfo;
         ActivityMarkerInfoBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_marker_info);
+
+        mSwipeRefreshLayout = findViewById(R.id.swipe_container_marker);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setColorSchemeResources(android.R.color.holo_green_light,
+                android.R.color.holo_blue_light,
+                android.R.color.holo_purple,
+                android.R.color.holo_orange_light);
+
         binding.setMarkerinfo(markInfo);
     }
 
@@ -41,5 +57,20 @@ public class MarkerInfoActivity extends AppCompatActivity implements MarkerInfoA
     protected void onDestroy() {
         super.onDestroy();
         markerInfoActivityPresenter.detachView();
+    }
+
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                markerInfoActivityPresenter.getMarkerInfo(latitude, longitude);
+                if (mSwipeRefreshLayout.isRefreshing()){
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+                Toast.makeText(MarkerInfoActivity.this, getResources().getString(R.string.downloaded), Toast.LENGTH_SHORT).show();
+
+            }
+        }, 4000);
     }
 }
